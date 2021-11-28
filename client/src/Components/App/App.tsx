55854c21196cfcc4f45b50334d9a021ad31234ed
Evgeniy <ekukonen@gmail.com>
2021-11-28 21:09:04 +0300

    client init

---
+++ client/src/Components/App/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+import Header from '../Header/Header';
+import Content from '../Content/Content';
+import Footer from '../Footer/Footer';
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
