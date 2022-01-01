32f36ab276a3c4f90b1447dc0701467915d892d6
Kukonen <ekukonen@gmail.com>
2022-01-01 22:39:54 +0300

    add Button component

---
+++ client/src/Components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {ButtonProps} from "./Button.props";
+import './Button.scss';
+import {Link} from "react-router-dom";
+import classNames from "classnames";
+
+const Button = ({children, link, fontSize}:ButtonProps) => {
+
+    if (link) {
+        return (
+            <Link to={link} className={
+                classNames(
+                    "Button",
+                    "ButtonLink",
+                    {
+                        fontSize: fontSize
+                    }
+                )}
+            >
+                {children}
+            </Link>
+        )
+    }
+
+    return (
+        <button className={
+            classNames(
+                "Button",
+                {
+                    fontSize:fontSize
+                }
+            )}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Button;
